2c5ed59f6a164df21502993ea723d29af082df0f
unknown <kdjambazov.bs@gmail.com>
2021-12-16 21:08:37 +0200

    Update forecast temperature units format.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=85685da8480ea03a812066c71ffd87bc&query=${latitude},${longitude}&units=f`
+    const url = `http://api.weatherstack.com/current?access_key=85685da8480ea03a812066c71ffd87bc&query=${latitude},${longitude}&units=m`
     
     request({url, json: true}, (error, response, body) => {
         if(error) {
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             callback(body.error.info)
         } else {
             const { weather_descriptions, temperature, precip } = body.current;
-            callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degress out. There is a ' + precip + '% chance of rain.')            
+            callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + 'C degress out. There is a ' + precip + '% chance of rain.')
         }      
     })
 }
